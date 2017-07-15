7d89294e4f0b9f0047b2a064f0b13521d64a493a
Mario <marioromero78@gmail.com>
2017-07-15 19:41:36 -0300

    add Strategy Pattern

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# design.patterns.js
+# Design Patterns in ES6 examples
+
+Design patterns examples taken from the "Head First - Design Pattern" book and translate to ES6 in a Node.js environment
+
+# Design Pattern: Strategy.
+Defines a family of algorithms, encapsulates each one, and make them interchangeable. Strategy lets the algorithm vary independently from the clients that use it.
+- Design Principle: "Identify the aspects of your application that vary and separate them
+from what stays the same." (cap1.p9)
+- Design Principle: "Program to an interface not an implementation." (cap1.p12)
+- Design Principle: "Favor composition over inheritance" (cap1.p23)
--- strategy/simUDuck.js
+++ strategy/simUDuck.js
@@ -1,5 +1,7 @@
 /*
-Design Pattern: Strategy
+Design Pattern: Strategy.
+Defines a family of algorithms, encapsulates each one, and make them interchangeable.
+Strategy lets the algorithm vary independently from the clients that use it.
 Design Principle: "Identify the aspects of your application that vary and separate them
 from what stays the same." (cap1.p9)
 Design Principle: "Program to an interface not an implementation." (cap1.p12)
@@ -7,42 +9,86 @@ Design Principle: "Favor composition over inheritance" (cap1.p23)
 */
 
 //SimUDuck App
-Class Duck() {
+class Duck {
   constructor () {
-
+    if (new.target === Duck) {
+     throw new TypeError("Cannot construct Abstract instances directly");
+   }
   }
+
   swim(){
     console.log("Swim!");
   }
+
   display(){
     console.log("I'm a Duck");
   }
+
   performQuack(){
     this.QuackBehavior.quack();
   }
+
   performFlight(){
     this.FlightBehavior.flight();
   }
+
   setFlightBehavior(fb){
     this.FlightBehavior = fb;
   }
+
   setQuackBehavior(qb){
     this.QuackBehavior = qb;
   }
 }
 
-Class MallardDuck extends Duck {}
-Class ReadHeadDuck extends Duck {}
-Class RubberDuck extends Duck {}
+class MallardDuck extends Duck {
+  constructor(){
+    super();
+    this.setFlightBehavior(new FlightWithWings());
+    this.setQuackBehavior(new Quack());
+  }
+
+  display(){
+      console.info("I'm MallardDuck");
+  }
+}
+
+class ReadHeadDuck extends Duck {
+  constructor(){
+    super();
+    this.setFlightBehavior(new FlightWithWings());
+    this.setQuackBehavior(new Quack());
+  }
+
+  display(){
+      console.info("I'm ReadHeadDuck");
+  }
+}
+
+class RubberDuck extends Duck {
+  constructor(){
+    super();
+    this.setFlightBehavior(new FlightNoWay());
+    this.setQuackBehavior(new Squeak());
+  }
+
+  display(){
+      console.info("I'm RubberDuck");
+  }
+}
 
 
 //Algorithms Flight
-Class FlightBehavior {
-  constructor(){}
+class FlightBehavior {
+  constructor(){
+    if (new.target === Duck) {
+     throw new TypeError("Cannot construct Abstract instances directly");
+   }
+  }
   flight(){}
 }
 
-Class FlightWithWings extends FlightBehavior {
+class FlightWithWings extends FlightBehavior {
   constructor(){
     super();
   }
@@ -51,23 +97,28 @@ Class FlightWithWings extends FlightBehavior {
     console.log("Flight with wings!");
   }
 }
-Class FlightNoWay extends FlightBehavior {
+
+class FlightNoWay extends FlightBehavior {
   constructor(){
     super();
   }
 
   flight(){
-    console.log("Flight with NO wings! :( )");
+    console.log("Flight with NO wings! :(");
   }
 }
 
 //Algorithms Quack
-Class QuackBehavior {
-  constructor(){}
+class QuackBehavior {
+  constructor(){
+    if (new.target === Duck) {
+     throw new TypeError("Cannot construct Abstract instances directly");
+   }
+  }
   quack(){}
 }
 
-Class Quack extends QuackBehavior {
+class Quack extends QuackBehavior {
   constructor(){
     super();
   }
@@ -77,7 +128,7 @@ Class Quack extends QuackBehavior {
   }
 }
 
-Class Squeak extends QuackBehavior {
+class Squeak extends QuackBehavior {
   constructor(){
     super();
   }
@@ -87,12 +138,37 @@ Class Squeak extends QuackBehavior {
   }
 }
 
-Class MuteQuack extends QuackBehavior {
+class MuteQuack extends QuackBehavior {
   constructor(){
     super();
   }
 
   quack(){
-    console.log("Say Nothing! :( )");
+    console.log("Say Nothing! :(");
   }
 }
+
+
+//Main
+const rubberDuck = new RubberDuck();
+rubberDuck.performFlight();
+rubberDuck.performQuack();
+rubberDuck.swim();
+rubberDuck.display();
+
+const mallardDuck = new MallardDuck();
+mallardDuck.performFlight();
+mallardDuck.performQuack();
+mallardDuck.swim();
+mallardDuck.display();
+
+const readHeadDuck = new ReadHeadDuck();
+readHeadDuck.performFlight();
+readHeadDuck.performQuack();
+readHeadDuck.swim();
+readHeadDuck.display();
+
+//Try to instanciate an "Abstract" class.
+//ES6 does not implement Abstract classes, so we mimic the exception manually.
+console.info('Testing the Abstract class exception...');
+const duck = new Duck();
